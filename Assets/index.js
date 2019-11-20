4cae597987a9e2947d0073c4209369167f4c60ad
luis.sales <luis.sales@navi.ifrn.edu.br>
2019-11-20 10:04:16 -0300

    Teste de JS

--- Assets/index.js
+++ Assets/index.js
@@ -5,18 +5,21 @@ $(function() {
             '1':'d-block',            
         }
     }
+   
     function showhidepopup(a,b){
         $.each(table[a]), function(i,elem){
             let c = elem[1-b];                
             $.each(elem), function(i,elem){
                 if(b.toString() == i){
-                    $("."+a).removeClass(Attr(elem)).addClass(c.childen[0]);                    
-                    $("."+a).Attr("data-pop",1-b);
+                    $(a).removeClass(attr(elem)).addClass(c.childen[0]);                    
+                    $(a).attr("data-pop",1-b);
                 }
             }
         }
     }
+    
     $('.call').click(function(){
-        showhidepopup("bg-pop",$(".bg-pop").data("pop"))
-    });
+        showhidepopup($(".bg-pop"),$(".bg-pop").attr("data-pop"));
+    })
+    
 });
---
+++ header.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-dark grad">
+    <a class="navbar-brand" href="index.php">ICS</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link text-light" href="index.php">Inicio <span class="sr-only">(página atual)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light" href="admReservas.php">Gerenciar Reservas</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link text-light" href="admQuartos.php">Gerenciar Quartos</a>
+            </li>
+            
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-link">
+                <a class="exit" href="index.php" data-toggle="sair">SAIR<span class="icon icon-exit"></span></a>                
+            </li>
+        </ul>
+    </div>
+</nav>
