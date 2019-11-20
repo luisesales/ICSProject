a689e9e719558af60c914215d53f72c103f4068c
luisesales <duduosales@hotmail.com>
2019-11-20 15:55:44 -0300

    Versão Finalizada de Js de Popup

--- Assets/index.js
+++ Assets/index.js
@@ -1,25 +1,23 @@
 $(function() {
     let table = {
-        'bg-pop':{
+        '0':{
             '0': 'd-none',
             '1':'d-block',            
         }
     }
    
-    function showhidepopup(a,b){
-        $.each(table[a]), function(i,elem){
-            let c = elem[1-b];                
-            $.each(elem), function(i,elem){
-                if(b.toString() == i){
-                    $(a).removeClass(attr(elem)).addClass(c.childen[0]);                    
-                    $(a).attr("data-pop",1-b);
+    function showhidepopup(a,b,c){        
+        $.each(table[a],function(i,elem){ 
+                if(b.toString() == i){               
+                    $(c).removeClass(elem)
+                    $(c).addClass(table[a][1-i]);                  
+                    $(c).attr("data-pop",1-b);
                 }
-            }
-        }
+            });
     }
     
     $('.call').click(function(){
-        showhidepopup($(".bg-pop"),$(".bg-pop").attr("data-pop"));
+        showhidepopup($(this).attr("data-function"),$(".bg-pop").attr("data-pop"),$(".bg-pop"));
     })
     
 });
