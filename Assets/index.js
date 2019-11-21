e7466ddf449ea25850ed5726aceb0fa7f30a9c5d
luisesales <duduosales@hotmail.com>
2019-11-21 03:43:05 -0300

    versão inicial da página de gerenciamento de quartos(quase completa)

--- Assets/index.js
+++ Assets/index.js
@@ -1,4 +1,5 @@
 $(function() {
+    $(".sub").hide();
     let table = {
         '0':{
             '0': 'd-none',
@@ -6,18 +7,25 @@ $(function() {
         }
     }
    
-    function showhidepopup(a,b,c){        
+    function changeclass(a,b,c,d){        
         $.each(table[a],function(i,elem){ 
-                if(b.toString() == i){               
-                    $(c).removeClass(elem)
-                    $(c).addClass(table[a][1-i]);                  
-                    $(c).attr("data-pop",1-b);
+                if(b.toString() == i){  
+                    $(c[d]).removeClass(elem)
+                    $(c[d]).addClass(table[a][1-i]);                  
+                    $(c[d]).attr("data-pop",1-b);
                 }
             });
     }
+    function ShowHideArea(a){
+        if($(a).is(":visible")){$(a).slideUp();}               
+            else {$(a).slideDown();}    
+    }
     
     $('.call').click(function(){
-        showhidepopup($(this).attr("data-function"),$(".bg-pop").attr("data-pop"),$(".bg-pop"));
+        changeclass($(this).attr("data-function"),$(".bg-pop").attr("data-pop"),$(".bg-pop"),$(this).attr("data-item"));
+    })    
+    $(".tri").click(function(){
+        ShowHideArea($(".sub"));
     })
     
 });
---
+++ Footer.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-dark grad mt-32 footer w-100">    
+        <div class="ml-auto">
+            <ul class="navbar nav">
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#"><span class="icon icon-face"></span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#"><span class="icon icon-google"></span></a>
+                </li>    
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#"><span class="icon icon-yout"></span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#"><span class="icon icon-whats"></span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#"><span class="icon icon-twitter"></span></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
