f173cce723ad056dab4acdde37ef49ffbbbae8cb
brunopcem <55712899+brunopcem@users.noreply.github.com>
2019-11-22 05:06:01 -0300

    coisas a +

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 	<?php 
         $mysqli = new mysqli('localhost', 'bruno', '123', 'hotel') or die(myslqi_error($mysqli));
         $result_quarto = $mysqli->query("SELECT * FROM Quarto") or die($mysqli->error);
+        $result_hospede = $mysqli->query("SELECT * FROM Hospede") or die($mysqli->error);
     ?>
 	<div data-pop="0" class="bg-pop z-2 w-100 h-100 d-none ">
         <div class="popup z-3 pt-8 pb-16 px-16 mx-auto text-dark w-50 border border-dark">
@@ -24,74 +25,58 @@
             <h4 class="col-auto order-sm-1 order-2">Insira os dados a Seguir</h4>
             <button type="button" data-function="0" data-item="0" class="close col-auto call ml-auto order-sm-2 order-1 mr-8" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
-                </button>            
+            </button>            
             </div>
+
             <div class="row">
-                <div class="col-lg-9 col-12 mt-8">
-                    <h5>Seu Nome</h5>
-                    <textarea class="w-100 s-32" data-input="nome"></textarea>
-                </div>
-                <div class="col-lg-3 col-sm-4  mt-8">
-                        <h5>Quarto</h5>
-                        <input type="text" class="w-100 s-32" disabled data-input="quarto">
-                    </div>                        
-                <div class="col-md-auto mt-8">
-                    <h5>Seu CPF</h5>
-                    <input type="text" class="w-100 s-32" data-input="cpf">
-                </div>
-                <div class="col-md-auto mt-8">
-                        <h5>Nº de Acompanhantes</h5>
-                        <input type="text" class="w-100 s-32 " data-input="acompanhantes">
-                    </div>                                            
+                <form action="process.php" method="POST">
+
+                    <div class="col-lg-9 col-12 mt-8">
+                        <h5>Seu Nome</h5>
+                        <input type="text" class="w-100 s-32" value="<?php echo $nome; ?>" name="nome">
+                    </div>
+
+                    <div class="col-md-auto mt-8">
+                        <h5>Seu CPF</h5>
+                        <input type="text" class="w-100 s-32" value="<?php echo $cpf; ?>" name="cpf">
+                    </div>
+
+                    <div class="col-md-auto mt-8">
+                        <h5>Nº de Pessoas</h5>
+                        <input type="number" class="w-100 s-32 " value="<?php echo $qtd_pessoas; ?>" name="qtd_pessoas">
+                    </div>
+                        <button type="submit" name="reservar">Reservar</button>
+                </form>
             </div>
-            <div class="row">
-                <div class="ml-auto col-sm-auto">
-                        <a href="index.php?reservar= <?php echo $row['id_quarto']; ?> " ><div class="btn btn-warning"><span class="px-8 py-8">Reservar</span></div></a>
-                </div>
-            </div>
+
         </div>
-	</div>
-	<div data-pop="0"  class="bg-pop z-2 w-100 h-100 d-none">
-            <div class="popup z-3 pt-8 pb-16 px-16 mx-auto text-dark w-25 border border-dark">            
-                <div class="row p-0">            
-                <h4 class="col-auto order-sm-1 order-2">Insira senha mestra</h4>
-                <button type="button" data-function="0" data-item="1" class="close col-auto call ml-auto order-sm-2 order-1 mr-8" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>            
-                </div>
-                <div class="row">
-                        <div class="col mt-8">
-                                <h5>ID</h5>
-                                <input type="text" class="w-100 s-32" data-input="id">
-                            </div>                                                    
-                </div>
-                <div class="row mt-16">
-                    <div class="ml-auto col-sm-auto">
-                            <a><div class="btn btn-warning"><span class="px-8 py-8">Atualizar</span></div></a>
-                    </div>
-                </div>
-            </div>
-		</div>
-		<div data-pop="0"  class="bg-pop z-2 w-100 h-100 d-none">
-                    <div class="popup z-3 pt-8 pb-16 px-16 mx-auto text-dark w-25 border border-dark">            
-                        <div class="row p-0">            
-                        <h4 class="col-auto order-sm-1 order-2">Insira senha mestra</h4>
-                        <button type="button" data-function="0" data-item="1" class="close col-auto call ml-auto order-sm-2 order-1 mr-8" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>            
-                        </div>
-                        <div class="row">
-                                <div class="col mt-8">                                
-                                        <input type="text" class="w-100 s-32" id="pass">
-                                    </div>                                                    
-                        </div>
-                        <div class="row mt-16">
-                            <div data-function="0" data-item="1" class="ml-auto call col-sm-auto log">
-                                    <a><div class="btn btn-warning"><span class="px-8 py-8">Entrar</span></div></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    </div>
+	<nav class="navbar navbar-expand-lg navbar-dark grad">
+	    <a class="navbar-brand" href="index.php">
+                    
+        </a>
+	    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+	        <span class="navbar-toggler-icon"></span>
+	    </button>
+
+	    <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+	        <ul class="navbar-nav mr-auto">
+	            <li class="nav-item">
+	                <a class="nav-link text-light" href="index.php">Inicio <span class="sr-only">(página atual)</span></a>
+	            </li>
+	            <li class="nav-item">
+	                <a class="nav-link text-light" href="admQuartos.php">Gerenciar Quartos</a>
+	            </li>
+	            
+	        </ul>
+
+	        <ul class="navbar-nav ml-auto">
+	            <li class="nav-link">
+	                <a class="exit" href="index.php" data-toggle="sair">SAIR<span class="icon icon-exit"></span></a>                
+	            </li>
+	        </ul>
+	    </div>
+	</nav>
 	<div class="container-fluid">
 	<div class="mt-24 row">
 		<?php while($row = $result_quarto->fetch_assoc()): ?>
@@ -100,7 +85,7 @@
 	        <div class="my-8 mx-8 row">
 	            <div class="col-xl-8 col-lg-6 my-auto">
 	            	<span>Quarto </span>
-	                <span> <?php echo $row['id'];?> </span>
+	                <span id="id_quarto"> <?php echo $row['id'];?> </span>
 	                <span>, </span>
 	                <span> <?php echo $row['estrelas'];?> </span>
 	                <img class="s-24" src="Assets/Icons/Star.png" >
@@ -113,9 +98,9 @@
 	                <span class="text-success"> <?php echo $row['valor_estadia'];?> </span> 
 	            </div>
 	            <div data-function="0" data-item="0" class="ml-auto col-sm-auto call">
-	                <a >
+	                <a href="process.php?reserva_r= <?php echo $row['id']; ?>">
 	                	<div class="btn btn-warning">
-	                		<span class="px-8 py-8">Reservar</span>
+	                		 <span class="px-8 py-8">Reservar</span>
 	                	</div>
 	                </a>
 				</div>
--- process.php
+++ process.php
@@ -1,5 +1,13 @@
 <?php
-    $mysqli = new mysqli('localhost', 'bruno', '123', 'Hotel') or die(myslqi_error($mysqli));
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+
+    $mysqli = new mysqli('localhost', 'bruno', '123', 'hotel') or die(myslqi_error($mysqli));
+
+    $result_quarto = $mysqli->query("SELECT * FROM Quarto") or die($mysqli->error);
+    $result_hospede = $mysqli->query("SELECT * FROM Hospede") or die($mysqli->error);
 
     $id_hospede = 0;
     $id_quarto = "";
@@ -8,7 +16,7 @@
 
     $nome = "";
     $cpf = "";
-    $qtd_pessoas = 0;
+    $qtd_pessoas = 1;
 
     $local = "";
     $valor_estadia = 0;
@@ -24,6 +32,28 @@
 
     $update = false;
 
-    
+    if (isset($_POST['reservar'])){
+        $nome = $_POST['nome'];
+        $cpf = $_POST['cpf'];
+        $qtd_pessoas = $_POST['qtd_pessoas'];
+        
+
+        $mysqli->query("INSERT INTO Hospede (nome, cpf, qtd_pessoas) VALUES ('$nome', '$cpf', $qtd_pessoas)") or die($mysqli->error());
+
+        header("location: index.php");
+    }
+
+    if(isset($_GET['reserva_r'])){
+
+        $quarto_id = $_GET['reserva_r'];
+        $hospede_id = ;
+
+        $mysqli->query("INSERT INTO Reserva(quarto_id, hospede_id, end) VALUES('$quarto_id', $hospede_id, 0)") or die($mysqli->error());
+
+        $_SESSION['message'] = "Deletado!";
+        $_SESSION['msg_type'] = "dngr";
+
+    header("location: index.php");
+}
     
 
