f6c54da5ac642b3ea80733d4d2b7aae48dcdc792
Luís Eduardo de Oliveira Sales <luis.sales@academico.ifrn.edu.br>
2019-12-06 16:22:45 -0300

    Refatoração header

--- Admin/header2.php
+++ Admin/header2.php
@@ -2,22 +2,13 @@
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação"> <span class="navbar-toggler-icon"></span>
 	</button>
 	<div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-		<ul data-pop="0" class="navbar-nav mr-auto d-none">
-			<li class="nav-item"> <a class="nav-link text-light" href="index.php">Inicio <span class="sr-only">(página atual)</span></a>
-			</li>
-			<li class="nav-item"> <a class="nav-link text-light" href="admQuartos.php">Gerenciar Quartos</a>
-			</li>
-		</ul>
-		<ul data-pop="0" class="navbar-nav ml-auto d-none">
-			<li data-function="1" class="nav-link change"> <a class="exit" data-toggle="sair">SAIR<span class="icon icon-exit "></span></a> 
-			</li>
-		</ul>
-		<ul data-pop="1" class="navbar-nav mr-auto">
+		
+		<ul  class="navbar-nav mr-auto">
 			<li class="nav-item"> <a class="nav-link text-light" href="index.php">Inicio <span class="sr-only">(página atual)</span></a>
 			</li>
 		</ul>
-		<ul data-pop="1" class="navbar-nav ml-auto">
-			<li data-function="0" data-item="2" class="nav-item call"> <a href="login.php" class="nav-link btn btn-outline-light enter">Entrar</a>
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item "> <a href="login.php" class="nav-link btn btn-outline-light">Entrar</a>
 			</li>
 		</ul>
 	</div>
--- Cliente/headerlogado.php
+++ Cliente/headerlogado.php
@@ -3,15 +3,15 @@
 	</button>
 	<div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
 		<ul data-pop="1" class="navbar-nav mr-auto">
-			<li class="nav-item"> <a class="nav-link text-light" href="admQuartos.php">Gerenciar Quartos</a>
+			<li class="nav-item"> <a class="nav-link text-light" href="../Admin/admQuartos.php">Gerenciar Quartos</a>
 			</li>
-			<li class="nav-item"> <a class="nav-link text-light" href="admReservas.php">Gerenciar Reservas</a>
+			<li class="nav-item"> <a class="nav-link text-light" href="../Admin/admReservas.php">Gerenciar Reservas</a>
 			</li>
-			<li class="nav-item"> <a class="nav-link text-light" href="admHospedes.php">Gerenciar Hospedes</a>
+			<li class="nav-item"> <a class="nav-link text-light" href="../Admin/admHospedes.php">Gerenciar Hospedes</a>
 			</li>
 		</ul>
 		<ul data-pop="1" class="navbar-nav ml-auto">
-			<li data-function="0" data-item="2" class="nav-item call"> <a href="index.php" class="exit">Sair<span class="icon icon-exit"></span></a>
+			<li class="nav-item "> <a href="index.php" class="exit">Sair<span class="icon icon-exit"></span></a>
 			</li>
 		</ul>
 	</div>
